390559806dbee3d24533403ec3a69f7817a45afa
Anton Borisov <borisov2011@gmail.com>
2023-08-24 15:34:19 +0700

    исправил пути к img

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 const products = [{
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "red",
-    img: "src/img/1.jpg"
-  }, {
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "green",
-    img: "src/img/2.jpg"
-  }, {
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "blue",
-    img: "src/img/3.jpg"
-  }, {
-    name: "Nike Metcon 2",
-    price: "130",
-    color: "black",
-    img: "src/img/4.jpg"
-  }, {
-    name: "Nike free run",
-    price: "170",
-    color: "black",
-    img: "src/img/7.jpg"
-  }, {
-    name: "Nike Metcon 3",
-    price: "150",
-    color: "green",
-    img: "src/img/5.jpg"
-  }];
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "red",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "green",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "blue",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg"
+}, {
+  name: "Nike Metcon 2",
+  price: "130",
+  color: "black",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg"
+}, {
+  name: "Nike free run",
+  price: "170",
+  color: "black",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/7.jpg"
+}, {
+  name: "Nike Metcon 3",
+  price: "150",
+  color: "green",
+  img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/5.jpg"
+}];
 
 const IconSwitch = ({ icon, onSwitch }) => {
   return (
